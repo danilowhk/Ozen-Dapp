2106694c555cac19277f76b980bc3742294db085
danilowhk <danilowhk@gmail.com>
2022-06-28 16:34:17 -0300

    First commit

---
+++ components/dataInfos.js
@@ -0,0 +1,17 @@
+export function dataInfo(){
+
+
+    return(
+        <>
+            <p>User Balance: 1000 OZT</p>
+            <p>Outflow: 80 OZT</p>
+            <p>Inflow: 120 OZT</p>
+
+            <p>Artist Sean Rad Balance: 1506 OZT</p>
+            
+            <p>DAO 1 : Balance</p>
+            <p>DAO 2: Balance</p>
+
+        </>
+    )
+}
